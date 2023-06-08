c78cfacced0237d10a61adaa8a389956f08f72ae
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-06-08 21:50:39 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { BCRYPT_PASSWORD, SALT_ROUNDS } = process.env;
+const { BCRYPT_PASSWORD, SALT_ROUNDS, TOKEN_SECRET } = process.env;
 
 export default {
 	pepper: BCRYPT_PASSWORD,
 	salt: SALT_ROUNDS,
+	tokenSecret: TOKEN_SECRET,
 };
