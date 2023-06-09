1f9f5d02ceb0247a92dffb945ffaa8646aaf1128
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-06-09 19:52:18 +0300

    update authentication middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,11 +2,11 @@ import { RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
 
 import config from '../config';
+import { db } from '../datastore/indexDao';
+import { JwtObject } from '../types';
+import { Indexed } from './custom';
 
-type Indexed = {
-	[key: string]: any;
-};
-export const isAuthenticated: RequestHandler = (
+export const isAuthenticated: RequestHandler = async (
 	req: Indexed,
 	res,
 	next
@@ -18,19 +18,27 @@ export const isAuthenticated: RequestHandler = (
 		}
 		const bearer = authorization && authorization.split(' ')[0];
 		const token = authorization && authorization.split(' ')[1];
-		if (bearer !== 'Bearer') {
+
+		if (bearer !== 'Bearer')
 			return res.status(400).send('Invalid token');
-		}
+
 		if (!token) return res.status(401).send('access denied');
 
-		const decodedPayload = jwt.verify(
+		const decodedPayload: JwtObject = jwt.verify(
 			token,
 			config.tokenSecret as unknown as string
-		);
+		) as JwtObject;
 		if (!decodedPayload) return res.status(401).send('Invalid token');
+		const user = await db.getUserByUsername(decodedPayload.username);
+
+		if (!user)
+			return res.status(401).json({
+				message: 'user not found',
+			});
+
 		req.user = decodedPayload;
 		next();
 	} catch (error) {
-		return res.status(401).send('access denied');
+		return res.status(401).send({ error: 'access denied' });
 	}
 };
