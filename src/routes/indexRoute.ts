1e090d4de5b7f150b73586e1575c9228f13ffa9f
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-06-10 14:43:32 +0300

    Update indexRoute.ts

--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -1,7 +1,8 @@
-import { Router } from 'express';
+import { Router, application } from 'express';
 
 import authRoutes from './APIs/authAPIs';
 import commentRoutes from './APIs/commentAPIs';
+import likeRoutes from './APIs/likeAPIs';
 import postRoutes from './APIs/postsAPIs';
 
 const routes = Router();
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use(authRoutes);
 routes.use('/posts', postRoutes);
 routes.use('/comments', commentRoutes);
+routes.use('/likes', likeRoutes);
 
 export default routes;
